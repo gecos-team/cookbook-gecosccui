d2a398c0431a21a4e34dc4781bc4eb3520666149
Alfonso E.M <alfonso@el-magnifico.org>
2016-01-11 12:58:41 +0100

    reading gecosccui backend version from environment variable

--- attributes/backend.rb
+++ attributes/backend.rb
@@ -9,9 +9,9 @@
 # http://www.osor.eu/eupl
 #
 
-#TODO: ask for version in install script
-default['gecoscc-ui']['backend']['version'] = '2.1.10'
-#default['gecoscc-ui']['backend']['version'] = 'development'
+#TODO: choose from available versions in install script
+#default['gecoscc-ui']['backend']['version'] = '2.1.10'
+default['gecoscc-ui']['backend']['version'] = ENV['GECOSCC_VER']
 default['gecoscc-ui']['backend']['package'] = 'https://github.com/gecos-team/gecoscc-ui/archive/' << default['gecoscc-ui']['backend']['version'] << '.tar.gz'
 default['gecoscc-ui']['backend']['virtual_prefix'] = '/opt/gecosccui-'
 default['gecoscc-ui']['backend']['workers'] = 2
